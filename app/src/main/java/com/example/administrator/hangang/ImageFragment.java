0de2982905783df098407ffd3b0159ab8bfaccd0
paeroo <paeroo0304@naver.com>
2018-09-16 17:29:24 +0900

    네비게이션 뷰 및 액션바 적용 완료 메인화면 이미지 프레그먼트로 바꿈

--- app/src/main/java/com/example/administrator/hangang/ImageFragment.java
+++ app/src/main/java/com/example/administrator/hangang/ImageFragment.java
@@ -2,28 +2,24 @@ package com.example.administrator.hangang;
 
 import android.content.Context;
 import android.net.Uri;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.support.v4.app.Fragment;
 
 
-public class MainFragment extends Fragment {
-    ImageButton button1;
-    @Nullable
-    @Override
+
+
+public class ImageFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        if(container!=null){
+        if (container != null) {
             container = null;
         }
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main,container,false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_image, container, false);
 
 //        Button button = (Button)rootView.findViewById(R.id.button);
 //        button.setOnClickListener(new View.OnClickListener(){
@@ -33,16 +29,6 @@ public class MainFragment extends Fragment {
 //                activity.onFragmentChanged(0);
 //            }
 //        });
-        button1 = (ImageButton)rootView.findViewById(R.id.MainButton1);
-        System.out.print(2);
-        button1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                System.out.print(3);
-                MainActivity activity = (MainActivity)getActivity();
-                activity.onFragmentChanged(0);
-            }
-        });
-        return rootView;
+    return rootView;
     }
 }
--- app/src/main/java/com/example/administrator/hangang/MainActivity.java
+++ app/src/main/java/com/example/administrator/hangang/MainActivity.java
@@ -4,10 +4,21 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    Fragment mainFragment;
+    ImageFragment imageFragment;
+    MainFragment mainFragment;
+
     Fragment searchFragment;
+    DrawerLayout drawerLayout;
+    ActionBar actionBar;
 
 
     @Override
@@ -16,12 +27,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mainFragment = new MainFragment();
+        imageFragment = new ImageFragment();
        /* searchFragment = new SearchFragment();*/
 
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+
+
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.add(R.id.imageFragment,imageFragment);
         fragmentTransaction.add(R.id.mainFragment, mainFragment);
         fragmentTransaction.commit();
+        actionBar = getSupportActionBar();
+        drawerLayout = (DrawerLayout) findViewById(R.id.draw_layout) ;
+        actionBar.setHomeAsUpIndicator(R.mipmap.baseline_dehaze_white_18dp);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("");
 
 
     }
@@ -32,5 +52,24 @@ public class MainActivity extends AppCompatActivity {
         }else if(index==1){
             getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment,mainFragment).commit();
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            if(!drawerLayout.isDrawerOpen(GravityCompat.START)){
+                drawerLayout.openDrawer(GravityCompat.START);
+            }else{
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
